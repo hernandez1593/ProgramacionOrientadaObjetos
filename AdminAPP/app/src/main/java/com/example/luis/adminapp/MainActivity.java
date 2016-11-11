e9ece371cb9857969f5a043ff8eaf0a3632a258f
Leiver Jimenez <leiver38hfh@gmail.com>
2016-11-11 01:20:57 -0600

    Vista principal y login admin

--- AdminAPP/app/src/main/java/com/example/luis/adminapp/MainActivity.java
+++ AdminAPP/app/src/main/java/com/example/luis/adminapp/MainActivity.java
@@ -1,13 +1,35 @@
 package com.example.luis.adminapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnLogin = (Button) findViewById(R.id.btnLogin);
+
+        btnLogin.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(getApplicationContext(),activityStockView.class);
+                startActivity(intent);
+            }
+        });
+
     }
+
+
+
+
+
 }
