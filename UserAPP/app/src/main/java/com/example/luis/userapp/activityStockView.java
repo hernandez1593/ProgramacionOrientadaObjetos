302653320124278ffebad4c0598f56adbfb3cf25
Alejandro <alejandro386@gmail.com>
2016-11-30 16:33:49 -0600

    user

---
+++ UserAPP/app/src/main/java/com/example/luis/userapp/activityStockView.java
@@ -0,0 +1,13 @@
+package com.example.luis.userapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class activityStockView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stock_view);
+    }
+}
