e92cae58b2840e141729cc95190d8f0bb7a7810b
Alejandro <alejandro386@gmail.com>
2016-11-30 17:38:12 -0600

    registro usuario sin llamada

---
+++ UserAPP/app/src/main/java/com/example/luis/userapp/ProductosService.java
@@ -0,0 +1,32 @@
+package com.example.luis.userapp;
+
+import java.util.List;
+
+import retrofit.Callback;
+import retrofit.http.Field;
+import retrofit.http.FormUrlEncoded;
+import retrofit.http.GET;
+import retrofit.http.POST;
+
+/**
+ * Created by Adrián on 30/11/2016.
+ */
+
+public interface ProductosService {
+
+    @GET("/productos")
+    void getProductos(Callback<List<Productos>> callback);
+
+    @FormUrlEncoded
+    @POST("/DellProducto")
+    void deleteProduct(@Field("name") String name, Callback<Boolean> callback);
+
+    @FormUrlEncoded
+    @POST("/AddProducto")
+    void addProduct(@Field("name") String name, @Field("description") String description,
+                    @Field("amount") int amount, @Field("price") int price, Callback<Boolean> callback);
+    @FormUrlEncoded
+    @POST("/UpdateProducto")
+    void editProduct(@Field("name") String name, @Field("description") String description,
+                     @Field("amount") int amount, @Field("price") int price, Callback<Boolean> callback);
+}
--- UserAPP/app/src/main/java/com/example/luis/userapp/RegisterActivity.java
+++ UserAPP/app/src/main/java/com/example/luis/userapp/RegisterActivity.java
@@ -2,12 +2,23 @@ package com.example.luis.userapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class RegisterActivity extends AppCompatActivity {
-
+    Button btnRegister;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        btnRegister = (Button) findViewById(R.id.btnRegister);
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
+
 }
