912c1117079dba4dd227cc9df22c1dabad77a243
Leiver Jimenez <leiver38hfh@gmail.com>
2016-11-30 11:49:11 -0600

    asda

---
+++ AdminAPP/app/src/main/java/com/example/luis/adminapp/Persona.java
@@ -0,0 +1,16 @@
+package com.example.luis.adminapp;
+
+/**
+ * Created by Adrián on 30/11/2016.
+ */
+
+public class Persona {
+    public String nombre;
+    public String login;
+    public String password;
+    public int telefono;
+    public int tipo;
+
+
+
+}
