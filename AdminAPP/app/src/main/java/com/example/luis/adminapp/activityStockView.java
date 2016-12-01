f5f27702262475672fa671db38dc7212a6cc29e8
Leiver Jimenez <leiver38hfh@gmail.com>
2016-11-30 19:38:24 -0600

    merge

--- AdminAPP/app/src/main/java/com/example/luis/adminapp/activityStockView.java
+++ AdminAPP/app/src/main/java/com/example/luis/adminapp/activityStockView.java
@@ -113,7 +113,7 @@ public class activityStockView extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.activitystockactions, menu);
-        
+
         return true;
     }
 
