937b9c536bc352a929473dbac90ef94deb282df1
Alejandro <alejandro386@gmail.com>
2016-11-30 18:15:47 -0600

    libs

--- UserAPP/app/src/main/java/com/example/luis/userapp/activityStockView.java
+++ UserAPP/app/src/main/java/com/example/luis/userapp/activityStockView.java
@@ -1,7 +1,10 @@
 package com.example.luis.userapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class activityStockView extends AppCompatActivity {
 
@@ -10,4 +13,25 @@ public class activityStockView extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stock_view);
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menulogout, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+
+
+        if(id == R.id.logout){
+            this.onDestroy();
+            super.onResume();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
 }
